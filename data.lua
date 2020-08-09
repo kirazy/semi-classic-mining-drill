9efdae635a1e4bf6528a30a842f7bf3cc57b0b34
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-08-08 18:36:54 -0700

    Initial commit

---
+++ data.lua
@@ -0,0 +1,6 @@
+-- Copyright (c) 2020 Kirazy
+-- Part of Classic Mining Drill
+--     
+-- See LICENSE.md in the project directory for license information.
+
+require("prototypes.electric-mining-drill")
